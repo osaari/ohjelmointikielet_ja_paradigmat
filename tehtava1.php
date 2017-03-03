f6d864261b68d6456f96dda40a3dad0e334a8ad5
Otso Saarikoski <otsosa@metropolia.fi>
2017-03-03 11:49:49 +0200

    First Commit

---
+++ tehtava1.php
@@ -0,0 +1,12 @@
+<?php
+
+$a < 10; 
+
+if ($a < 10) {
+	echo "a on pienempi kuin 10 <br />";
+	
+} else {
+	echo "a on 10 tai enemmän <br />";
+}
+
+?>
---
+++ tehtava3.php
@@ -0,0 +1,12 @@
+<?php
+
+$numerot = array(1,2,3,4,5);
+print_r($numerot);
+echo "<br>";
+
+foreach($numerot as $arvo) {
+echo $arvo;
+}
+//echo $numerot[5];
+
+?>
